1e4435428fbcf49aa855c2926c0055edcb65e459
Kon-UU <92216445+Kon-UU@users.noreply.github.com>
2022-06-03 14:37:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#이차함수를 그리고 Y=0 과의 교점 확인해보기
 import turtle
 X_CHUK = 300 #X축
-X_V2 = 1 #2차항
-X_V1 = 2 #1차항
-V_0 = 1 #상수항
-X_SCALEUP = 10 #X축 곱하기 (가시성)
+X_V2 = 20 #2차항
+X_V1 = 5 #1차항
+V_0 = 90 #상수항
+X_SCALEUP = 20 #X축 곱하기 (가시성)
 Y_SCALEDOWN = 5 #Y축 나누기 (가시성)
 rsult_dict = []
 
@@ -38,20 +39,30 @@ for i, rsult in enumerate(rsult_dict):
         if int(rsult) == 0:
             mTurtle.goto(X_SCALEUP * (i - 0.5 * (screenx)), 0)
             mTurtle.dot(10, "red")
-        elif rsult_dict[i - 1] < 0 < rsult_dict[i] or rsult_dict[i] > 0 > rsult_dict[i + 1]:
+        elif rsult_dict[i - 1] < 0 < rsult_dict[i] or rsult_dict[i] > 0 > rsult_dict[i + 1]: #전 좌표와 비교하여 그 사이에 0이 존재 하면 점 찍기
             mTurtle.goto(X_SCALEUP * (i - 0.5 * (screenx)), 0)
             mTurtle.dot(10, "red")
-    except:
+    except: #i + 1 에서 마지막에 오류가 발생 하기에 오류 시 무시를 넣어줌
         pass
 
 #판별식
-bsq4ac = (X_V1 ** 2) - 4 * X_V2 * V_0 
+bsq4ac = (X_V1 ** 2) - 4 * X_V2 * V_0 #b^2-4ac
 if bsq4ac > 0:
-    print("두 점에서 접한다.")
+    mTurtle.write("두 점에서 만난다.", align="left", font=("Arial", 40, "normal"))
+    print("두 점에서 만난다.")
 elif bsq4ac == 0:
+    mTurtle.write("한 점에서 접한다.", align="left", font=("Arial", 40, "normal"))
     print("한 점에서 접한다.")
 else:
+    mTurtle.write("접하지 않는다.", align="left", font=("Arial", 40, "normal"))
     print("접하지 않는다.")
 
+mTurtle.goto(0, -210) 
+mTurtle.write(f"D={bsq4ac}", align="left", font=("Arial", 40, "normal")) #판별식 표시
+
+#식 print
+mTurtle.goto(0, -250)
+mTurtle.write(f"{X_V2}X²{'{0:+d}'.format(X_V1)}{'{0:+d}'.format(V_0)}", align="left", font=("Arial", 40, "normal"))
+
 turtle.update()
-turtle.mainloop()
+turtle.mainloop() #안꺼지게
